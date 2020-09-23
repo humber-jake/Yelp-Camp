081691deb570f1c02a1b8b328f4ed14b811e21e7
humber-jake <humber.jake@gmail.com>
2020-09-23 23:37:35 +0000

    Add navbar and temporary styling

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.get("/", function(req, res){
 
 let campgrounds = [
 		{name: "Squamish Riverside", image: "https://www.photosforclass.com/download/px_2398220"},
+		{name: "Thousand Islands, Ganonoque", image: "https://images.pexels.com/photos/2662816/pexels-photo-2662816.jpeg?auto=compress&cs=tinysrgb&h=350"},{name: "Squamish Riverside", image: "https://www.photosforclass.com/download/px_2398220"},
+		{name: "Thousand Islands, Ganonoque", image: "https://images.pexels.com/photos/2662816/pexels-photo-2662816.jpeg?auto=compress&cs=tinysrgb&h=350"},{name: "Squamish Riverside", image: "https://www.photosforclass.com/download/px_2398220"},
+		{name: "Thousand Islands, Ganonoque", image: "https://images.pexels.com/photos/2662816/pexels-photo-2662816.jpeg?auto=compress&cs=tinysrgb&h=350"},{name: "Squamish Riverside", image: "https://www.photosforclass.com/download/px_2398220"},
+		{name: "Thousand Islands, Ganonoque", image: "https://images.pexels.com/photos/2662816/pexels-photo-2662816.jpeg?auto=compress&cs=tinysrgb&h=350"},{name: "Squamish Riverside", image: "https://www.photosforclass.com/download/px_2398220"},
 		{name: "Thousand Islands, Ganonoque", image: "https://images.pexels.com/photos/2662816/pexels-photo-2662816.jpeg?auto=compress&cs=tinysrgb&h=350"},
 		{name: "Oka Riverside", image: "https://pixabay.com/get/53e4d1424b56a814f1dc84609620367d1c3ed9e04e507749752873d29e48c6_340.jpg"},
 	]
