cf5271ef7988b8b88496f5a0f225727787663ecf
humber-jake <humber.jake@gmail.com>
2020-09-25 00:20:40 +0000

    Add database and new campground functionality

--- app.js
+++ app.js
@@ -1,35 +1,64 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp", {
+	 useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to DB!'))
+.catch(error => console.log(error.message));
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create({
+// 	name: "Thousand Islands, Ganonoque", 
+// 	image: "https://images.pexels.com/photos/2662816/pexels-photo-2662816.jpeg?auto=compress&cs=tinysrgb&h=350"
+// }, function(err, campground){
+// 	if(err){
+// 		console.log("Something went wrong.");
+// 		console.log(err);
+// 	} else {
+// 		console.log("Campground added.");
+// 		console.log(campground);
+// 	}
+// });
 
 app.get("/", function(req, res){
 	res.render("landing");
 });
 
-let campgrounds = [
-		{name: "Squamish Riverside", image: "https://www.photosforclass.com/download/px_2398220"},
-		{name: "Thousand Islands, Ganonoque", image: "https://images.pexels.com/photos/2662816/pexels-photo-2662816.jpeg?auto=compress&cs=tinysrgb&h=350"},{name: "Squamish Riverside", image: "https://www.photosforclass.com/download/px_2398220"},
-		{name: "Thousand Islands, Ganonoque", image: "https://images.pexels.com/photos/2662816/pexels-photo-2662816.jpeg?auto=compress&cs=tinysrgb&h=350"},{name: "Squamish Riverside", image: "https://www.photosforclass.com/download/px_2398220"},
-		{name: "Thousand Islands, Ganonoque", image: "https://images.pexels.com/photos/2662816/pexels-photo-2662816.jpeg?auto=compress&cs=tinysrgb&h=350"},{name: "Squamish Riverside", image: "https://www.photosforclass.com/download/px_2398220"},
-		{name: "Thousand Islands, Ganonoque", image: "https://images.pexels.com/photos/2662816/pexels-photo-2662816.jpeg?auto=compress&cs=tinysrgb&h=350"},{name: "Squamish Riverside", image: "https://www.photosforclass.com/download/px_2398220"},
-		{name: "Thousand Islands, Ganonoque", image: "https://images.pexels.com/photos/2662816/pexels-photo-2662816.jpeg?auto=compress&cs=tinysrgb&h=350"},
-		{name: "Oka Riverside", image: "https://pixabay.com/get/53e4d1424b56a814f1dc84609620367d1c3ed9e04e507749752873d29e48c6_340.jpg"},
-	]
-
 app.get("/campgrounds", function(req, res){
-	res.render("campgrounds", {campgrounds: campgrounds});
+	Campground.find({}, function(err, allCampgrounds){
+		if(err){
+			console.log(err);
+		} else {
+			res.render("campgrounds", {campgrounds: allCampgrounds});
+		}
+	});
+
 })
 
 app.post("/campgrounds", function(req, res){
 	let name = req.body.name;
 	let image = req.body.image;
 	let newCampground = {name: name, image: image};
-	campgrounds.push(newCampground);
-	res.redirect("/campgrounds");
+	Campground.create(newCampground, function(err, newCamp){
+		if(err){
+			console.log(err);
+		} else {
+			res.redirect("/campgrounds");
+		}
+	});	
 })
 
 app.get("/campgrounds/new", function(req, res){
